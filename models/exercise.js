2418be585129feef3fc240b7df3058860f479479
Sai Swarup Yakkala | Intern | Vivriti Asset Management <Sai.Intern@vivritiamc.com>
2023-08-07 16:44:36 +0530

    Changed exercise database schema

--- models/exercise.js
+++ models/exercise.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  images: [{ type: String }],
+  time: { type: Number, required: true },
+  image_urls: [{ type: String }],
 });
 
 const Exercise = mongoose.model('Exercise', exerciseSchema);
